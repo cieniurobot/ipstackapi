f73566a4edd056f7d2cec983f714e4375b5359bd
Marcin Plociennik <mplociennik@trans.eu>
2019-11-15 20:33:51 +0100

    chore: configure tslint

--- src/api/rest/v1/routes/geolocation.ts
+++ src/api/rest/v1/routes/geolocation.ts
@@ -10,23 +10,23 @@ geolocationRouter.use(function timeLog(req, res, next) {
 
 geolocationRouter.get('/:id', (req, res) => {
     const id = req.params.id;
-    res.json(`Hello id: ${id}`)
+    res.json(`Hello id: ${id}`);
 });
 
 geolocationRouter.get('/', (req, res) => {
-    res.json({"message": "Hello World!"})
+    res.json({"message": "Hello World!"});
 });
 
 geolocationRouter.post('/', (req, res) => {
-    res.send('Got a POST request')
+    res.send('Got a POST request');
 });
 
 geolocationRouter.put('/', (req, res) => {
-    res.send('Got a PUT request at /user')
+    res.send('Got a PUT request at /user');
 });
 
 geolocationRouter.delete('/', (req, res) => {
-    res.send('Got a DELETE request at /user')
+    res.send('Got a DELETE request at /user');
 });
 
 export default geolocationRouter;
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -2,7 +2,7 @@ import config from 'config';
 import jwt from 'jsonwebtoken';
 
 
-export default class UserService{
+export default class UserService {
     private users = [{ id: 1, username: 'test', password: 'test', firstName: 'Test', lastName: 'User' }];
 
     async authenticate(username: string, password: string) {
