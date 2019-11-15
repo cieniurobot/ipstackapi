8db2a044a4cb93794b23caf201e625343f4b2fcc
Marcin Plociennik <mplociennik@trans.eu>
2019-11-15 19:51:22 +0100

    chore: create user service

---
+++ src/services/UserService.ts
@@ -0,0 +1,19 @@
+import config from 'config';
+import jwt from 'jsonwebtoken';
+
+
+export default class UserService{
+    private users = [{ id: 1, username: 'test', password: 'test', firstName: 'Test', lastName: 'User' }];
+
+    async authenticate(username: string, password: string) {
+        const user = this.users.find(u => u.username === username && u.password === password);
+        if (user) {
+            const token = jwt.sign({ sub: user.id }, config.get('api.secret'));
+            const { password, ...userWithoutPassword } = user;
+            return {
+                ...userWithoutPassword,
+                token
+            };
+        }
+    }
+}
