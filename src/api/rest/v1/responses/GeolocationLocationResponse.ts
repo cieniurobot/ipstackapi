854ff4be4bdcea187652139012d4b532953491f3
Marcin Plociennik <mplociennik@trans.eu>
2019-11-16 15:05:00 +0100

    chore: create geo responses

---
+++ src/api/rest/v1/responses/GeolocationLocationResponse.ts
@@ -0,0 +1,38 @@
+import GeolocationLocationResponseInterface from "../interfaces/GeolocationLocationResponseInterface";
+import GeolocationLocationLanguageResponseInterface from "../interfaces/GeollocationLocationLanguageResponseInterface";
+import GeolocationLocation from "../../../../db/models/GeolocationLocation";
+import GeolocationLocationLanguageResponse from "./GeolocationLocationLanguageResponse";
+import GeolocationLocationGeolocationLocationLanguage
+    from "../../../../db/models/GeolocationLocationGeolocationLocationLanguage";
+
+
+export default class GeolocationLocationResponse implements GeolocationLocationResponseInterface {
+    geoname_id: number;
+    capital: string;
+    country_flag: string;
+    country_flag_emoji: string;
+    country_flag_emoji_unicode: string;
+    calling_code: string;
+    is_eu: boolean;
+    languages: GeolocationLocationLanguageResponseInterface[];
+    created_at: Date;
+    updated_at: Date;
+
+    constructor(attrs: GeolocationLocation) {
+        this.geoname_id = attrs.geoname_id;
+        this.capital = attrs.capital;
+        this.country_flag = attrs.country_flag;
+        this.country_flag_emoji = attrs.country_flag_emoji;
+        this.country_flag_emoji_unicode = attrs.country_flag_emoji_unicode;
+        this.calling_code = attrs.calling_code;
+        this.is_eu = attrs.is_eu;
+        this.languages = this.mapLanguagesToResponses(attrs.languages);
+    }
+
+    mapLanguagesToResponses(languages: GeolocationLocationGeolocationLocationLanguage[]): Array<GeolocationLocationLanguageResponse> {
+        const languagesResponses = languages.map((item) => {
+            return new GeolocationLocationLanguageResponse(item.language);
+        });
+        return languagesResponses;
+    }
+}
---
+++ src/api/rest/v1/responses/GeolocationResponse.ts
@@ -0,0 +1,41 @@
+import GeolocationLocationResponseInterface from "../interfaces/GeolocationLocationResponseInterface";
+import GeolocationLocationResponse from "./GeolocationLocationResponse";
+import Geolocation from "../../../../db/models/Geolocation";
+import GeolocationResponseInterface from "../interfaces/GeolocationResponseInterface";
+
+
+export default class GeolocationResponse implements GeolocationResponseInterface {
+    ip: string;
+    type: string;
+    continent_code: string;
+    continent_name: string;
+    country_code: string;
+    country_name: string;
+    region_code: string;
+    region_name: string;
+    city: string;
+    zip: string;
+    latitude: number;
+    longtitude: number;
+    location: GeolocationLocationResponseInterface;
+    created_at: Date;
+    updated_at: Date;
+
+    constructor(attrs: Geolocation) {
+        this.ip = attrs.ip;
+        this.type = attrs.type;
+        this.continent_code = attrs.continent_code;
+        this.continent_name = attrs.continent_name;
+        this.country_code = attrs.country_code;
+        this.country_name = attrs.country_name;
+        this.region_code = attrs.region_code;
+        this.region_name = attrs.region_name;
+        this.city = attrs.city;
+        this.zip = attrs.zip;
+        this.latitude = attrs.latitude;
+        this.longtitude = attrs.longtitude;
+        this.location = new GeolocationLocationResponse(attrs.location);
+        this.created_at = attrs.created_at;
+        this.updated_at = attrs.updated_at;
+    }
+}
