71d20c2dabcc7ecf9b3c7aad4ac637c2fde39b38
Marcin Plociennik <mplociennik@trans.eu>
2019-11-17 20:25:48 +0100

    chore: update router post method

--- src/api/rest/v1/routes/GeolocationRouter.ts
+++ src/api/rest/v1/routes/GeolocationRouter.ts
@@ -2,6 +2,9 @@ import logger from "../../../../logger";
 import express, {NextFunction, Request, Response} from "express";
 import GeolocationResponse from "../responses/GeolocationResponse";
 import GeolocationRepository from "../../../../db/repositories/GeolocationRepository";
+import Geolocation from "../../../../db/models/Geolocation";
+import {check, validationResult} from "express-validator";
+import GeolocationRequest from "../requests/GeolocationRequest";
 
 const geolocationRouter = express.Router();
 const geolocationRepository = new GeolocationRepository();
@@ -16,16 +19,55 @@ geolocationRouter.get('/', (req, res) => {
     res.json({"message": "Hello World!"});
 });
 
-geolocationRouter.post('/', (req, res) => {
-    res.send('Got a POST request');
+geolocationRouter.post('/', [
+    check('ip').not().isEmpty(),
+    check('type').not().isEmpty(),
+    check('continent_code').not().isEmpty(),
+    check('continent_name').not().isEmpty(),
+    check('country_code').not().isEmpty(),
+    check('country_name').not().isEmpty(),
+    check('region_code').not().isEmpty(),
+    check('region_name').not().isEmpty(),
+    check('city').not().isEmpty(),
+    check('zip').not().isEmpty(),
+    check('latitude').isNumeric(),
+    check('longitude').isNumeric(),
+    check('location.geoname_id').not().isEmpty(),
+    check('location.capital').not().isEmpty(),
+    check('location.capital').not().isEmpty(),
+    check('location.languages_ids').isArray(),
+    check('location.country_flag').not().isEmpty(),
+    check('location.country_flag_emoji').not().isEmpty(),
+    check('location.country_flag_emoji_unicode').not().isEmpty(),
+    check('location.calling_code').not().isEmpty(),
+    check('location.is_eu').not().isEmpty(),
+], async (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({errors: errors.array()});
+    }
+    const geolocation = await geolocationRepository.create(new GeolocationRequest(req.body));
+    res.json(new GeolocationResponse(geolocation));
+    return next();
 });
 
 geolocationRouter.put('/', (req, res) => {
     res.send('Got a PUT request at /user');
 });
 
-geolocationRouter.delete('/', (req, res) => {
-    res.send('Got a DELETE request at /user');
+geolocationRouter.delete('/:id', async (req, res, next) => {
+    const geolocation = await Geolocation.findByPk(parseInt(req.params.id));
+    if (geolocation) {
+        logger.debug(`geolocation: ${JSON.stringify(geolocation.get({plain: true}))}`);
+        await geolocation.destroy();
+        res.json({
+            message: `Geolocation deleted!`
+        });
+        return next();
+    }
+    res.status(404).json({
+        message: `Geolocation not found! ID: ${req.params.id}`
+    });
 });
 
 geolocationRouter.get('/:id', async (req, res, next) => {
@@ -36,7 +78,7 @@ geolocationRouter.get('/:id', async (req, res, next) => {
         });
     }
 
-    const data = await geolocationRepository.getByPk(parseInt(req.params.id));
+    const data = await geolocationRepository.findFullByPk(parseInt(req.params.id));
     if (data) {
         const geolocationResponse = new GeolocationResponse(data);
         res.status(200).json(geolocationResponse);
