8aea76acfcc83a47bdfc1ce942809c02b474c2ed
Marcin Plociennik <mplociennik@trans.eu>
2019-11-17 20:24:45 +0100

    chore: create rest interfaces

---
+++ src/api/rest/v1/interfaces/GeolocationLocationRequestInterface.ts
@@ -0,0 +1,10 @@
+export default interface GeolocationLocationRequestInterface {
+    geoname_id: number;
+    capital: string;
+    languages_ids: number[];
+    country_flag: string;
+    country_flag_emoji: string;
+    country_flag_emoji_unicode: string;
+    calling_code: string;
+    is_eu: boolean;
+}
