fa1030ce862ba643efd1e4f640e4ccdf81e515a4
Marcin Plociennik <mplociennik@trans.eu>
2019-11-18 20:18:03 +0100

    chore: refactor migration

--- migrations/20191115082943-create-geolocation-location.js
+++ migrations/20191115082943-create-geolocation-location.js
@@ -2,47 +2,48 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable('g_location', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      geoname_id: {
-        type: Sequelize.INTEGER
-      },
-      capital: {
-        type: Sequelize.STRING
-      },
-      country_flag: {
-        type: Sequelize.STRING
-      },
-      country_flag_emoji: {
-        type: Sequelize.STRING
-      },
-      country_flag_emoji_unicode: {
-        type: Sequelize.STRING
-      },
-      calling_code: {
-        type: Sequelize.STRING
-      },
-      is_eu: {
-        type: Sequelize.BOOLEAN
-      },
-      geolocation_id: {
-        type: Sequelize.INTEGER,
-        onDelete: 'cascade',
-        references: { model: 'geolocation', key: 'id' }
-      },
-      created_at: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updated_at: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-    });
+          id: {
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: Sequelize.INTEGER
+          },
+          geoname_id: {
+            type: Sequelize.INTEGER
+          },
+          capital: {
+            type: Sequelize.STRING
+          },
+          country_flag: {
+            type: Sequelize.STRING
+          },
+          country_flag_emoji: {
+            type: Sequelize.STRING
+          },
+          country_flag_emoji_unicode: {
+            type: Sequelize.STRING,
+          },
+          calling_code: {
+            type: Sequelize.STRING
+          },
+          is_eu: {
+            type: Sequelize.BOOLEAN
+          },
+          geolocation_id: {
+            type: Sequelize.INTEGER,
+            onDelete: 'cascade',
+            references: {model: 'geolocation', key: 'id'}
+          },
+          created_at: {
+            allowNull: false,
+            type: Sequelize.DATE
+          },
+          updated_at: {
+            allowNull: false,
+            type: Sequelize.DATE
+          }
+        }
+    );
   },
   down: (queryInterface, Sequelize) => {
     return queryInterface.dropTable('g_location');
